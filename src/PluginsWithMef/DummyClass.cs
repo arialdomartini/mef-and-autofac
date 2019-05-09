arialdomartini/neat-coin

A dummy class with a passing dummy test

src/NeatCoin/NeatCoin/DummyClass.cs
@@ -1,0 +1,6 @@
+﻿namespace NeatCoin
+{
+    public class DummyClass
+    {
+    }
+}

src/NeatCoin/NeatCoinTest/DummyTest.cs
@@ -1,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace NeatCoinTest
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void should_pass()
+        {
+            "friends".Should().Be("friends");
+        }
+    }
+}

arialdomartini/mef-and-autofac

src/PluginsWithMef/DummyClass.cs
@@ -1,0 +1,6 @@
+﻿namespace PluginsWithMef
+{
+    public class DummyClass
+    {
+    }
+}

src/PluginsWithMefTest/DummyTest.cs
@@ -1,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace PluginsWithMefTest
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void should_pass()
+        {
+            "friends".Should().Be("friends");
+        }
+    }
+}